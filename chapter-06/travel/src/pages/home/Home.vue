4c8445cae53c17718fc19a5f18fd56cd446d3965
heatdeath <632668308@qq.com>
2019-03-24 14:11:05 +0800

    7-1 header区域开发 + 7-2 iconfont的使用

--- chapter-06/travel/src/pages/home/Home.vue
+++ chapter-06/travel/src/pages/home/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <home-header></home-header>
+    <router-link to="/list">列表页</router-link>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ chapter-06/travel/src/pages/list/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    list-vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style></style>
