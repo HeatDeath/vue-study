12827ca407765888f6ace91b1e306450ecab854d
heatdeath <632668308@qq.com>
2019-03-26 09:20:17 +0800

    第9章 项目实战 - 旅游网站详情页面开发[finish]

--- chapter-06/travel/src/router/index.js
+++ chapter-06/travel/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -16,6 +17,15 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     }
-  ]
+  ],
+  // 每次路由切换时，xy轴初始位置为0
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
